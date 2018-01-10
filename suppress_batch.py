7f7c243cad16b5c1dd65e4e53d0013219c71e7d4
hutchesonn <hutchesonn@gmail.com>
2018-01-10 17:21:24 -0600

    Add files via upload

--- suppress_batch.py
+++ suppress_batch.py
@@ -12,7 +12,7 @@ authenticate = requests.post("{}/users/{}/login?password={}".format(aspace_url, 
 token = authenticate["session"]
 headers = {"X-ArchivesSpace-Session":token}
 
-enum_val_ids = ["273", "274", "275"]
+enum_val_ids = ["904", "913", "907", "916", "917", "295", "296", "297", "298", "304", "305", "306", "307", "308", "309", "310", "311", "312", "313", "314", "315", "316", "1341", "1342", "1343", "1340", "348", "231", "232", "234", "235", "241", "243", "237", "238", "239", "363", "358", "359", "360", "362", "273", "276", "279", "280", "281", "282"]
 for enumeration_val_id in enum_val_ids:
     enum_val_id = {':id' : int(enumeration_val_id),
                 'suppressed': True }
